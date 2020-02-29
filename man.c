906868c94b866099eb505228da31b3ecf1fc8d72
SH-Dmitriy <61613130+SH-Dmitriy@users.noreply.github.com>
2020-02-29 09:44:38 +0300

    Update man.c

--- man.c
+++ man.c
@@ -1,1 +1,36 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5
+#define M 5
+int main()
+{
+   int a[N][M];
+   for (int i = 0; i < N; i++)
+   {
+       for (int j = 0; j < M; j++)
+       {
+           a[i][j] = ( ((i == 0)  | (i == 4)) | ((j == 0) | (j == 4)) ) ? 1 : 0;
+           printf("%d ", a[i][j]);
+       }
+      printf("\n");
+   }
+   for (int i = 0; i < N; i++)
+   {
+       for (int j = 0; j < M; j++)
+       {
+           if (a[i][j] == 0)
+           a[i][j] = (rand()%2);
+       }
+      printf("\n");
+   }
+   for (int i = 0; i < N; i++)
+   {
+       for (int j = 0; j < M; j++)
+       {
+           printf("%d ", a[i][j]);
+       }
+      printf("\n");
+   }
+   getchar();
+   return 0;
+}
